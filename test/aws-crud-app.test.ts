d69c4686e83010acf9da18a44de901d988777d6c
Sunday Ezeilo <ezeilo.su@gmail.com>
2023-07-20 09:43:43 +0100

    Initial commit

---
+++ test/aws-crud-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCrudApp from '../lib/aws-crud-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-crud-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCrudApp.AwsCrudAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
